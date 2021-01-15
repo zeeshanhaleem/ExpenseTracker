3ea7cceeb5c7a4e694c9d17fa88373d11ea24abb
zeeshanhaleem <zeeshanhaleem142@gmail.com>
2021-01-15 18:24:29 +0500

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-         Expense Tracker   
+         Expense Tracker by Zeeshan Haleem   
         </h2>
     )
 }
